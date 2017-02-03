5cda6d78b639209a3590e1a2a82df270880e47bc
jdbsilva <dinis.joel@gmail.com>
2017-02-03 20:16:00 +0000

    small change

--- getstatus.c
+++ getstatus.c
@@ -137,14 +137,12 @@ void get_volume(void)
     snd_ctl_elem_id_t *id;
     snd_ctl_elem_value_t *control;
 
-// To find card and subdevice: /proc/asound/, aplay -L, amixer controls
     snd_hctl_open(&hctl, "hw:0", 0);
     snd_hctl_load(hctl);
 
     snd_ctl_elem_id_alloca(&id);
     snd_ctl_elem_id_set_interface(id, SND_CTL_ELEM_IFACE_MIXER);
 
-// amixer controls
     snd_ctl_elem_id_set_name(id, "Master Playback Volume");
 
     snd_hctl_elem_t *elem = snd_hctl_find_elem(hctl, id);
